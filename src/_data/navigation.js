318d161b1c5614cef920aa5b4bb697bf26a92dec
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-11-08 17:28:34 +0000

    content edits to the main pages, minor UI updates, font preloading

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -22,10 +22,10 @@ export default {
       text: 'Style guide',
       url: '/styleguide/'
     },
-    {
+    /*{
       text: 'Imprint',
       url: '/imprint/'
-    },
+    },*/
     {
       text: 'Privacy',
       url: '/privacy/'
